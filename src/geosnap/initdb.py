39ecbb320a10fea2d0148254be018b348c5d650c
cackharot <cackharot@gmail.com>
2014-11-13 19:02:20 +0530

    updated init db script

--- src/geosnap/initdb.py
+++ src/geosnap/initdb.py
@@ -16,12 +16,12 @@ def setup():
     item = user_service.get_by_email("admin@geosnap.in")
 
     if item:
-        user_service.delete(item['_id'])
-
-    print("Create admin user")
-    item = {"name": "admin", "username": 'admin', "email": 'admin@geosnap.in', "registered_ip": "10.0.0.1",
-            'roles': ['super_admin', 'admin'], 'password': 'pass@123'}
-    user_service.create(item)
+        print("Create admin user")
+        item = {"name": "admin", "username": 'admin', "email": 'admin@geosnap.in', "registered_ip": "10.0.0.1",
+                'roles': ['super_admin', 'admin'], 'password': 'pass@123'}
+        user_service.create(item)
+    else:
+        print(item)
 
 if __name__ == "__main__":
     print("Initializing database...")
