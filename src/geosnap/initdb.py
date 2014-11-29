737d00cfd1c5a58efbb888321295616df4cd7fc9
cackharot <cackharot@gmail.com>
2014-11-29 17:21:39 +0530

    --amend

--- src/geosnap/initdb.py
+++ src/geosnap/initdb.py
@@ -15,17 +15,18 @@ def setup():
     user_service = UserService(db)
 
     item = user_service.get_by_email("admin@geosnap.in")
-
-    if item:
-        print('Deleting previous admin user')
-        user_service.delete(str(item['_id']))
-
     print("Creating admin user...")
     api_key = "LKND89&%#@!NDFHLKD"
-    item = {"name": "admin", "username": 'admin', "email": 'admin@geosnap.in', "registered_ip": "10.0.0.1",
-            'roles': ['super_admin', 'admin'], 'password': 'pass@123',
-            'api_key': api_key}
-    user_service.create(item)
+
+    if not item:
+        item = {"name": "admin", "username": 'admin', "email": 'admin@geosnap.in', "registered_ip": "10.0.0.1",
+                'roles': ['super_admin', 'admin'], 'password': 'pass@123',
+                'api_key': api_key}
+        user_service.create(item)
+    else:
+        if not 'api_key' in item:
+            item['api_key'] = api_key
+        user_service.update(item)
     print(item)
 
 
